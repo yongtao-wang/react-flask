3dfe7f7b6f9217ce1f13b462ff9bb0cff41433fb
Yongtao Wang <yongtao.wang.16@gmail.com>
2023-03-20 15:07:04 +0000

    Removed unwanted sql scripts

--- backend/src/script/database_setup.sql
+++ backend/src/script/database_setup.sql
@@ -13,27 +13,3 @@ CREATE SCHEMA travel CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE travel;
 
 -- SQLAlchemy will create the table for you
--- Thus the following commands are not necessary
-
--- Create article table
-CREATE TABLE article (
-  id int NOT NULL AUTO_INCREMENT,
-  title VARCHAR(200),
-  content LONGTEXT,
-  created_on DATETIME DEFAULT CURRENT_TIMESTAMP,
-  last_updated DATETIME DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (id)
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Create user table
-CREATE TABLE user (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(200),
-  first_name VARCHAR(100),
-  last_name VARCHAR(100),
-  title VARCHAR(100),
-  email VARCHAR(100),
-  updated DATETIME DEFAULT CURRENT_TIMESTAMP,
-  deleted BOOLEAN DEFAULT 0,
-  PRIMARY KEY (id)
- ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
